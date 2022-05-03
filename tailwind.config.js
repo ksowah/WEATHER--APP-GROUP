8432696f8007b392bbb1e77cdad832266b5278f7
ksowah <sowahkelvin640@gmail.com>
2022-05-03 12:27:07 +0000

    add to tw config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: { colors: {
+      'black-rgba': 'rgba(0, 0, 0, 0.3)',
+      'very-dark-rgba': 'rgba(0, 0, 0, 0.7)',
+      'dark-rgba': 'rgba(8, 14, 44, 0.5)',
+    },},
+  },
+  plugins: [],
+}
