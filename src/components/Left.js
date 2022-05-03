370d2e1945ce14e7861755b349a06bdcd68ed4f5
ksowah <sowahkelvin640@gmail.com>
2022-05-03 12:28:43 +0000

    create left component

---
+++ src/components/Left.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Left = ({temp, city, summery, icon}) => {
+	return (
+		<div
+			className="w-2/3 h-full"
+			style={{
+				backgroundImage: `url(https://ksets.netlify.app/Group/group-weather.jpg)`,
+				backgroundSize: "cover",
+			}}
+		>
+			<div className="w-full h-full bg-black-rgba py-8 text-gray-200 flex flex-col justify-between">
+				<p className="text-sm pl-8">binary.beasts</p>
+
+				<div className="flex pl-8 bg-very-dark-rgba items-center space-x-4">
+					<h2 className="text-[8rem] font-semibold">{temp?.toFixed(1)}°</h2>
+
+					<span className="flex flex-col">
+						<h2 className="text-[3.5rem] font-normal">{city}</h2>
+						<p className="text-sm ">06:09 - Monday, 9 Sep '19</p>
+					</span>
+
+					<span className="flex flex-col items-center mt-7">
+
+					<img src={`http://openweathermap.org/img/w/${icon}.png`} alt="weather icon" className="animate-bounce"/>
+                     
+                        <h3 className="text-2xl font-light">{summery}</h3>
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Left;
